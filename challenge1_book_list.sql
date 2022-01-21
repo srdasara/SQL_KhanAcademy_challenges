e4edb8b3b188f5f6f5f5bd2ae3d2caa1c670911f
Srishti Dasarathy <37392984+srdasara@users.noreply.github.com>
2022-01-21 17:31:43 -0500

    Update challenge1_book_list.sql

--- challenge1_book_list.sql
+++ challenge1_book_list.sql
@@ -4,10 +4,8 @@ Create a table to store a list of books. It should have columns for id, name, an
 Add three of your favorite books into the table.
 Answer */
 
-
 CREATE TABLE books (id INTEGER PRIMARY KEY, name TEXT, rating INTEGER);
 
 INSERT INTO books VALUES (1, "Hands-on Machine Learning with Scikit-Learn, Keras & TensorFlow", 5);
 INSERT INTO books VALUES (2, "Sams Teach Yourself SQL in 10 Minutes", 5);
 INSERT INTO books VALUES (3, "Python Data Science", 4);
-
